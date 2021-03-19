bf7bc259f739da3a19b68e557dc5466632b780b3
malegarcal <mgarciac@outlook.it>
2021-03-19 11:46:04 -0300

    fixing workspace

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from "./components/navbar";
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "React";
+
 
 const Navbar = () => {
     return ( 
